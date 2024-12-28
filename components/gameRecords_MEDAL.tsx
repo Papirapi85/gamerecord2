e478b4b2e026c926965a9d40e8f66af6b3579b9f
PC1 <umdomby@yandex.ru>
2024-12-28 20:55:44 +0300

    edit

--- components/gameRecords_MEDAL.tsx
+++ components/gameRecords_MEDAL.tsx
@@ -30,9 +30,9 @@ export const GameRecord_MEDAL: React.FC<Props> = ({medals, countMedals}) => {
             {medals.map((medal, index) => (
                 <div key={index}>
                     <span className="ml-2">{medal.productName}:</span>
-                    <span className="ml-2">gold {medal.gold !== null && "gold " + medal.gold.user.fullName + " " + medal.gold.timestate.substring(3)}</span>
-                    <span className="ml-2">silver {medal.silver !== null && "silver " + medal.silver.user.fullName + " " + medal.silver.timestate.substring(3)}</span>
-                    <span className="ml-2">bronze {medal.bronze !== null && "bronze " + medal.bronze.user.fullName + " " + medal.bronze.timestate.substring(3)}</span>
+                    <span className="ml-2">{medal.gold !== null && "gold " + medal.gold.user.fullName + " " + medal.gold.timestate.substring(3)}</span>
+                    <span className="ml-2">{medal.silver !== null && "silver " + medal.silver.user.fullName + " " + medal.silver.timestate.substring(3)}</span>
+                    <span className="ml-2">{medal.bronze !== null && "bronze " + medal.bronze.user.fullName + " " + medal.bronze.timestate.substring(3)}</span>
                 </div>
             ))}
         </div>
