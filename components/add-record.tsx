90c5ff38b007b91dfac6ad8556a584b78fbebbe9
PC1 <umdomby@yandex.ru>
2024-12-31 15:17:56 +0300

    edit

--- components/add-record.tsx
+++ components/add-record.tsx
@@ -77,15 +77,15 @@ export const AddRecord: React.FC<Props> = ({user, category, product, productItem
 
     const selectFile = async (e: any) => {
         if (e.target.files[0]) {
-            if (e.target.files[0].size > 2 * 1000 * 1024) {
+            if (e.target.files[0].size > 5 * 1000 * 1024) {
                 const options = {
                     maxSizeMB: 2, // Максимальный размер в мегабайтах
                     maxWidthOrHeight: 1920, // Максимальная ширина или высота
                     useWebWorker: true, // Использовать веб-воркеры для повышения производительности
                 };
-                const compressedFile = await imageCompression(e.target.files[0], options);
-                const data = new FormData();
-                data.append('image', compressedFile, e.target.files[0].name)
+                //const compressedFile = await imageCompression(e.target.files[0], options);
+                //const data = new FormData();
+                data.append('image', e.target.files[0], e.target.files[0].name)
                 setFormData(data)
                 imgRef.current = true;
 
