a6c37113ad92b249da771868d7d16c75c86beb1f
PC1 <umdomby@yandex.ru>
2025-01-06 06:32:23 +0300

    edit

--- components/sheet-driver-left.tsx
+++ components/sheet-driver-left.tsx
@@ -17,19 +17,29 @@ import React from "react";
 export function SheetDriverLeft() {
     return (
         <Sheet>
-            <SheetTrigger asChild>
-                <ArrowBigRightDash />
+            <SheetTrigger  asChild>
+                <Button className="h-8 bg-gray-600 text-black ml-1" >
+                    <div style={{cursor: "pointer", fontSize: "12px",}}>CONTACTS</div>
+                </Button>
+
+                {/*<ArrowBigRightDash />*/}
             </SheetTrigger>
-            <SheetContent side="left">
+            <SheetContent className="bg-secondary" side="left">
                 <SheetHeader>
-                    <SheetTitle>MENU</SheetTitle>
+                    <SheetTitle>Contacts</SheetTitle>
                     <SheetDescription>
-                        LEFT
+                        Telegram @navatar85
+                    </SheetDescription>
+                    <SheetDescription>
+                        Email: umdom33@gmail.com
+                    </SheetDescription>
+                    <SheetDescription>
+                        Viber: +375333814578
                     </SheetDescription>
                 </SheetHeader>
                 <div className="grid gap-4 py-4">
 
-                    {/*<div className="grid grid-cols-4 items-center gap-4">*/}
+                {/*<div className="grid grid-cols-4 items-center gap-4">*/}
                     {/*    <Label htmlFor="name" className="text-right">*/}
                     {/*        Name*/}
                     {/*    </Label>*/}
